ddfdac75e7d7f8f5da18b4ece8e70a8f4c619f4f
Mics66 <92332836+Mics66@users.noreply.github.com>
2024-04-11 22:56:36 +0800

    Update guessing quiz.py

--- guessing quiz.py
+++ guessing quiz.py
@@ -1,29 +1,42 @@
 from time import sleep
+
+import os
+# Clearing the Screen
+
 print ("      ~ WHO AM I? ~         ")
 sleep(1)
 print ("(Bible Character Quiz Game) \n")
 
-sleep(1) 
+sleep(2) 
 playing = input("Do you want to start guessing who's who in the Bible?\nType 'yes' to proceed: ")
 if playing.lower() != "yes":
    quit()
+sleep(1)
+os.system('cls')
 
+#-----------------NAME INPUT--------------------
 name = input("\nPlease type your name: ")
 sleep(1)  
+os.system('cls')
+
 print("\nGet ready with your Bible knowledge! \n")
-print("Welcome to EASY level, " + name + ".""\n 10 questions :)\n")
+sleep(2)
+os.system('cls')
+
+print("Welcome to EASY level " + name + ".""\n 10 questions :)\n")
 sleep(1) 
 print(name + ", you have to guess 8 or more characters to proceed to the INTERMEDIATE LEVEL")
 playing = input("Type 'ok' if you understand: ")
 if playing.lower() != "ok":
     quit()
-score1 = 0
-sleep(2)
+os.system('cls')
 
 '''
 ------------------------------EASY LEVEL Q&A------------------------------
 -------------------------------10 QUESTIONS-------------------------------
 '''
+sleep(1)
+score1 = 0
 #1
 answer = input("\nWho's the first man that God created? ")
 if answer.lower() == "adam":
@@ -31,6 +44,8 @@ if answer.lower() == "adam":
   score1 += 1  
 else:
   print("The correct answer is 'Adam'")
+  sleep(2)
+os.system('cls')
 #2  
 answer = input("\nWho did God ask to make an ark? ")
 if answer.lower() == "noah":
@@ -38,6 +53,8 @@ if answer.lower() == "noah":
   score1 += 1  
 else:
   print("The correct answer is 'Noah'")
+  sleep(2)
+os.system('cls')
 #3  
 answer = input("\nWho's known as the 'Father of Many Nations'? ")
 if answer.lower() == "abraham":
@@ -45,6 +62,8 @@ if answer.lower() == "abraham":
   score1 += 1  
 else:
   print("The correct answer is 'Abraham'")
+  sleep(2)
+os.system('cls')
 #4  
 answer = input("\nWho did God use to part the Red sea? ")
 if answer.lower() == "moses":
@@ -52,6 +71,8 @@ if answer.lower() == "moses":
   score1 += 1  
 else:
   print("The correct answer is 'Moses'")
+  sleep(2)
+os.system('cls')
 #5  
 answer = input("\nWho got swallowed by a whale and stayed at it's belly for 3 days? ")
 if answer.lower() == "jonah":
@@ -59,6 +80,8 @@ if answer.lower() == "jonah":
   score1 += 1  
 else:
   print("The correct answer is 'Jonah'") 
+  sleep(2)
+os.system('cls')
 #6  
 answer = input("\nWho did God send to bring salvation to earth? ")
 if answer.lower() == "jesus":
@@ -66,6 +89,8 @@ if answer.lower() == "jesus":
   score1 += 1  
 else:
   print("The correct answer is 'Jesus'") 
+  sleep(2)
+os.system('cls')
 #7  
 answer = input("\nWho in the 12 apostles betrayed Jesus? ")
 if answer.lower() == "judah":
@@ -73,6 +98,8 @@ if answer.lower() == "judah":
   score1 += 1  
 else:
   print("The correct answer is 'Judah'")  
+  sleep(2)
+os.system('cls')
 #8  
 answer = input("\nWho's the father of 12 tribes? ")
 if answer.lower() == "israel":
@@ -80,6 +107,8 @@ if answer.lower() == "israel":
   score1 += 1  
 else:
   print("The correct answer is 'Israel'")  
+  sleep(2)
+os.system('cls')
 #9    
 answer = input("\nWho defeated a giant named 'Goliath'? ")
 if answer.lower() == "david":
@@ -87,6 +116,8 @@ if answer.lower() == "david":
   score1 += 1  
 else:
   print("The correct answer is 'David'")
+  sleep(2)
+os.system('cls')
 #10  
 answer = input("\nWho did God gifted with wisdom and wealth which later on had 700 wives and 300 concubines? ")
 if answer.lower() == "solomon":
@@ -94,29 +125,35 @@ if answer.lower() == "solomon":
   score1 += 1  
 else:
   print("The correct answer is 'Solomon'")
-
+  sleep(2)
+os.system('cls')
 
 #---------------------------EASY LEVEL SCORE--------------------------------
 sleep(1)
-print(name + "\n, you've guessed " + str(score1) + " correct characters out of 10.")
-sleep(.75)
+print("\n"+ name + ", you've guessed " + str(score1) + " correct characters out of 10.")
+sleep(2)
 if (score1) >= 8:
-  print ("\nWelcome to INTERMEDIATE level " + name + ".""\n 15 questions ;)\n")
+  os.system('cls')
+  print ("\nWelcome to INTERMEDIATE level " + name + ".""\n 15 questions ;)\n") 
+  sleep(1)
 else:
   print ("Don't forget to read the Bible!")
+  sleep(2)
   quit()
+os.system('cls')
 
 print(name + ", you have to guess 12 or more characters to proceed to the ADVANCED LEVEL")
 playing = input("Type 'ok' if you understand: ")
 if playing.lower() != "ok":
     quit()
-score2 = 0
-sleep(2)
+os.system('cls')
 
 '''
 ---------------------------INTERMEDIATE LEVEL Q&A---------------------------
 -------------------------------15 QUESTIONS---------------------------------
 '''
+score2 = 0
+sleep(1)
 
 #1
 answer = input("\nWho's the first king of Israel? ")
@@ -125,6 +162,8 @@ if answer.lower() == "saul":
   score2 += 1  
 else:
   print("The correct answer is 'Saul'")
+  sleep(2)
+os.system('cls')
 #2  
 answer = input("\nWho was thrown into the lion's den? ")
 if answer.lower() == "daniel":
@@ -132,6 +171,8 @@ if answer.lower() == "daniel":
   score2 += 1  
 else:
   print("The correct answer is 'Daniel'")
+  sleep(2)
+os.system('cls')
 #3
 answer = input("\nWho pretended to be his brother and stole his brother's inheritance right? ")
 if answer.lower() == "jacob":
@@ -139,6 +180,8 @@ if answer.lower() == "jacob":
   score2 += 1  
 else:
   print("The correct answer is 'Jacob'")
+  sleep(2)
+os.system('cls')
 #4
 answer = input("\nWho is Moses' brother which assisted him in leading Isrealites? ")
 if answer.lower() == "aaron":
@@ -146,6 +189,8 @@ if answer.lower() == "aaron":
   score2 += 1  
 else:
   print("The correct answer is 'Aaron'") 
+  sleep(2)
+os.system('cls')
 #5   
 answer = input("\nWho protected the two spies sent by Joshua to Jericho? ")
 if answer.lower() == "rahab":
@@ -153,6 +198,8 @@ if answer.lower() == "rahab":
   score2 += 1  
 else:
   print("The correct answer is 'Rahab'")
+  sleep(2)
+os.system('cls')
 #6   
 answer = input("\nWho is the prophet of God that was fed by ravens? ")
 if answer.lower() == "elijah":
@@ -160,6 +207,8 @@ if answer.lower() == "elijah":
   score2 += 1  
 else:
   print("The correct answer is 'Elijah'")   
+  sleep(2)
+os.system('cls')
 #7
 answer = input("\nWho led the Israelites into Canaan and conquered Jericho? ")
 if answer.lower() == "joshua":
@@ -167,6 +216,8 @@ if answer.lower() == "joshua":
   score2 += 1  
 else:
   print("The correct answer is 'Joshua'")
+  sleep(2)
+os.system('cls')
 #8
 answer = input("\nWho led the 300 warriors? ")
 if answer.lower() == "gideon":
@@ -174,6 +225,8 @@ if answer.lower() == "gideon":
   score2 += 1  
 else:
   print("The correct answer is 'Gideon'")
+  sleep(2)
+os.system('cls')
 #9
 answer = input("\nWho was sold as a slave by his brothers who later on became the interpretator of the dreams of the pharoh of Egypt? ")
 if answer.lower() == "joseph":
@@ -181,6 +234,8 @@ if answer.lower() == "joseph":
   score2 += 1  
 else:
   print("The correct answer is 'Joseph'")
+  sleep(2)
+os.system('cls')
 #10
 answer = input("\nWho had a great strength which comes from his long hair? ")
 if answer.lower() == "samson":
@@ -188,6 +243,8 @@ if answer.lower() == "samson":
   score2 += 1  
 else:
   print("The correct answer is 'Samson'")
+  sleep(2)
+os.system('cls')
 #11
 answer = input("\nWho is the prophet that anoints both Saul and David as king? ")
 if answer.lower() == "samuel":
@@ -195,6 +252,8 @@ if answer.lower() == "samuel":
   score2 += 1  
 else:
   print("The correct answer is 'Samuel'")
+  sleep(2)
+os.system('cls')
 #12
 answer = input("\nWho is a shepard that murdered his younger brother out of jealousy? ")
 if answer.lower() == "cain":
@@ -202,6 +261,8 @@ if answer.lower() == "cain":
   score2 += 1  
 else:
   print("The correct answer is 'Cain'")
+  sleep(2)
+os.system('cls')
 #13
 answer = input("\nWho is the Philistine beauty that deceived Samson to reveal the source of his strength? ")
 if answer.lower() == "delilah":
@@ -209,6 +270,8 @@ if answer.lower() == "delilah":
   score2 += 1  
 else:
   print("The correct answer is 'Delilah'")
+  sleep(2)
+os.system('cls')
 #14
 answer = input("\nWho traded his birthright to his younger brother for some soup? ")
 if answer.lower() == "esau":
@@ -216,6 +279,8 @@ if answer.lower() == "esau":
   score2 += 1  
 else:
   print("The correct answer is 'Esau'")
+  sleep(2)
+os.system('cls')
 #15 
 answer = input("\nWho got pregnant at the late age of 99 years old? ")
 if answer.lower() == "sarah":
@@ -223,30 +288,34 @@ if answer.lower() == "sarah":
   score2 += 1  
 else:
   print("The correct answer is 'Sarah'")
-
+  sleep(2)
+os.system('cls')
 
 #-----------------------INTERMEDIATE LEVEL SCORE----------------------------------
 sleep(1)
-print("\nYou've guessed " + str(score2) + " correct characters out of 15")
-sleep(.75)
+print("\n"+name+", you've guessed " + str(score2) + " correct characters out of 15")
+sleep(2)
 if (score2) >= 12:
-  print ("\nWelcome to ADVANCED level "+ name + ".""\n 20 questions 0_0)")
+  print("\nWelcome to ADVANCED level "+ name + ".""\n 20 questions 0_0)") 
+  sleep(1)
 else:
-  print ("Don't forget to read the Bible!")
+  print("Don't forget to read the Bible!") 
+  sleep(2)
   quit()
 
 print("\nThis will be the final round.\nContinue to showcase your knowledge in Bible.")
 playing = input("Type 'ok' if you understand: ")
 if playing.lower() != "ok":
     quit()
-
-score3 = 0
-sleep(2)
+os.system('cls')
 
 '''
 -------------------------ADVANCED LEVEL Q&A------------------------------
 ---------------------------20 QUESTIONS----------------------------------
 '''
+
+score3 = 0
+sleep(1)
 
 #1
 answer = input("\nWho was originally married to Uriah but charmed David while she was taking a bath? ")
@@ -255,6 +324,8 @@ if answer.lower() == "bathsheba":
   score3 += 1  
 else:
   print("The correct answer is 'Bathsheba'")
+  sleep(2)
+os.system('cls')
 #2  
 answer = input("\nWho was was intoxicated by his two daughters for him to impregnate them? ")
 if answer.lower() == "lot":
@@ -262,6 +333,8 @@ if answer.lower() == "lot":
   score3 += 1  
 else:
   print("The correct answer is 'Lot'")
+  sleep(2)
+os.system('cls')
 #3
 answer = input("\nWho became the new queen of Persia and saved her nation from being exiled? ")
 if answer.lower() == "esther":
@@ -269,6 +342,8 @@ if answer.lower() == "esther":
   score3 += 1  
 else:
   print("The correct answer is 'Esther'")
+  sleep(2)
+os.system('cls')
 #4
 answer = input("\n(answer format: ____ & ____)\nWho are the daughters of Laban that got married to Jacob? ")
 if answer.lower() == "leah & rachel":
@@ -279,6 +354,8 @@ elif answer.lower() == "rachel & leah":
   score3 += 1
 else:
   print("The correct answer is 'Rachel & Leah'") 
+  sleep(2)
+os.system('cls')
 #5   
 answer = input("\nWho was a prophetess and also the only female judge? ")
 if answer.lower() == "deborah":
@@ -286,6 +363,8 @@ if answer.lower() == "deborah":
   score3 += 1  
 else:
   print("The correct answer is 'Deborah'")
+  sleep(2)
+os.system('cls')
 #6   
 answer = input("\nWho is the daughter-in-law of Naomi that got married to Boaz? ")
 if answer.lower() == "ruth":
@@ -293,6 +372,8 @@ if answer.lower() == "ruth":
   score3 += 1  
 else:
   print("The correct answer is 'Ruth'")   
+  sleep(2)
+os.system('cls')
 #7
 answer = input("\nWho is the only daughter of Jacob that was raped by Shechem? ")
 if answer.lower() == "dina":
@@ -300,6 +381,8 @@ if answer.lower() == "dina":
   score3 += 1  
 else:
   print("The correct answer is 'Dina'")
+  sleep(2)
+os.system('cls')
 #8
 answer = input("\nWho's the 12th son of Jacob that was framed by Joseph as a robber? ")
 if answer.lower() == "benjamin":
@@ -307,6 +390,8 @@ if answer.lower() == "benjamin":
   score3 += 1  
 else:
   print("The correct answer is 'Benjamin'")
+  sleep(2)
+os.system('cls')
 #9
 answer = input("\nWho's known as the 'weeping prophet'? ")
 if answer.lower() == "jeremiah":
@@ -314,6 +399,8 @@ if answer.lower() == "jeremiah":
   score3 += 1  
 else:
   print("The correct answer is 'Jeremiah'")
+  sleep(2)
+os.system('cls')
 #10
 answer = input("\nWho got accused to be drunk as she was praying and weeping to God by a priest in the temple? ")
 if answer.lower() == "hannah":
@@ -321,6 +408,8 @@ if answer.lower() == "hannah":
   score3 += 1  
 else:
   print("The correct answer is 'Hannah'")
+  sleep(2)
+os.system('cls')
 #11
 answer = input("\nWho lives the longest years? ")
 if answer.lower() == "methuselah":
@@ -328,6 +417,8 @@ if answer.lower() == "methuselah":
   score3 += 1  
 else:
   print("The correct answer is 'Methuselah'")
+  sleep(2)
+os.system('cls')
 #12
 answer = input("\nWho fell to the ground from the third story window as he was sinking into a deep sleep while Paul was talking? ")
 if answer.lower() == "eutychus":
@@ -335,6 +426,8 @@ if answer.lower() == "eutychus":
   score3 += 1  
 else:
   print("The correct answer is 'Eutychus'")
+  sleep(2)
+os.system('cls')
 #13
 answer = input("\nWho is the priest and judge that took supervision over samuel? ")
 if answer.lower() == "eli":
@@ -342,6 +435,8 @@ if answer.lower() == "eli":
   score3 += 1  
 else:
   print("The correct answer is 'Eli'")
+  sleep(2)
+os.system('cls')
 #14
 answer = input("\nWho received the double portion power of Elijah? ")
 if answer.lower() == "elisha":
@@ -349,6 +444,8 @@ if answer.lower() == "elisha":
   score3 += 1  
 else:
   print("The correct answer is 'Elisha'")
+  sleep(2)
+os.system('cls')
 #15 
 answer = input("\nWho would not die before he had seen the Lord Messiah as revealed to him by the Holy Spirit? ")
 if answer.lower() == "simeon":
@@ -356,6 +453,8 @@ if answer.lower() == "simeon":
   score3 += 1  
 else:
   print("The correct answer is 'Simeon'")
+  sleep(2)
+os.system('cls')
 #16
 answer = input("\nWho prepared a special gallows to be used for the excution of Mordecai, but later on was hanged by that same gallows? ")
 if answer.lower() == "haman":
@@ -363,6 +462,8 @@ if answer.lower() == "haman":
   score3 += 1  
 else:
   print("The correct answer is 'Haman'")
+  sleep(2)
+os.system('cls')
 #17
 answer = input("\nWho's the high priest at the time of trial and crucifixion of Jesus Christ? ")
 if answer.lower() == "caiaphas":
@@ -370,6 +471,8 @@ if answer.lower() == "caiaphas":
   score3 += 1  
 else:
   print("The correct answer is 'Caiaphas'")
+  sleep(2)
+os.system('cls')
 #18
 answer = input("\nWho got a vision from God and sent two servants and a soldier to bring Peter to him? ")
 if answer.lower() == "cornelius":
@@ -377,6 +480,8 @@ if answer.lower() == "cornelius":
   score3 += 1  
 else:
   print("The correct answer is 'Cornelius'")
+  sleep(2)
+os.system('cls')
 #19
 answer = input("\nWho despised David in her heart when she saw him leaping and dancing before the Lord? ")
 if answer.lower() == "michal":
@@ -384,7 +489,9 @@ if answer.lower() == "michal":
   score3 += 1  
 else:
   print("The correct answer is 'Michal'")
-#120
+  sleep(2)
+os.system('cls')
+#20
 answer = input("\n(answer format: ____ & ____)\nWho are the two couple who lied to Peter about the money of their sold field that fell down and died? ")
 if answer.lower() == "ananias & sapphira":
   print ("Correct!")
@@ -394,7 +501,8 @@ elif answer.lower() == "sapphira & ananias":
   score3 += 1  
 else:
   print("The correct answer is 'Ananias & Sapphira'")
-
+  sleep(2)
+os.system('cls')
 
 #---------------------------ADVANCED LEVEL SCORE------------------------------
 sleep(1)
@@ -404,6 +512,8 @@ print("Total guesses of "+ name + " is " + str(fscore) + " out of 45 questions.\
 sleep(3)
 if (score3) >= 18:
   print ("Continue to read the Bible and learn more.")
+  sleep(2)
 else:
   print ("Don't forget to read the Bible!")
+  sleep(2)
   quit()
